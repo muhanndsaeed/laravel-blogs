4dc20715785435de4f980c65a45ede876ad79a68
Muhannad Saeed Almutairi <muhnndsaeed@Muhannads-MacBook-Pro.local>
2023-07-23 13:14:37 +0300

    create AuthController

---
+++ app/Http/Controllers/API/Authentication/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API\Authentication;
+
+use App\Http\Controllers\API\BaseController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends BaseController
+{
+    //
+}
