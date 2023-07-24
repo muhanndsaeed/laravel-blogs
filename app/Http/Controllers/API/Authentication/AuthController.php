4495e8b54d75fb0377873a511f1be3de2fa4abd9
Noura Saad <noura.saad1419@gmail.com>
2023-07-24 11:03:10 +0300

    feat : Create and migrate model user

--- app/Http/Controllers/API/Authentication/AuthController.php
+++ app/Http/Controllers/API/Authentication/AuthController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\API\Authentication;
 
-use App\Http\Controllers\API\BaseController;
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\BaseController;
 
 class AuthController extends BaseController
 {
-    //
+    
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role',['admin','agent','user'])->default('user');
+            $table->string('phone_number');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
