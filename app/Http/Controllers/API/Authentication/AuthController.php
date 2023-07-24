eb38fd4108a9ad2a9d1e5c79e734ed4662d5c09f
Noura Saad <noura.saad1419@gmail.com>
2023-07-24 15:58:31 +0300

    feat : as a user i can login by email and password

--- app/Http/Controllers/API/Authentication/AuthController.php
+++ app/Http/Controllers/API/Authentication/AuthController.php
@@ -10,4 +10,25 @@ use App\Http\Controllers\API\BaseController;
 class AuthController extends BaseController
 {
     
+   
+    public function login(Request $request){
+        $validatedData = $request -> validate([
+            'email'=> 'required',
+            'password' => 'required',
+        ]);
+        
+        //The attempt method accepts an array of key / value pairs as its first argument. The values in the array will be used to find the user in your database table
+        if(!auth() -> attempt($validatedData )){
+            return $this->handleError('invalid email or password',401);
+        }else{
+            $result = [
+                'user' => auth() -> user(),
+                'accessToken' => auth() -> user()->createToken('authToken')->accessToken
+            ];
+            return $this->handleResponse($result,'Login successfully');
+            
+        }
+    }
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -19,7 +20,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'user_name',
         'email',
         'phone_number',
         'role',
