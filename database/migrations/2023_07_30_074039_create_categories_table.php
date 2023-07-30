4529a10e1147638f4a2fddd3612a6ef519b19447
Muhannad Saeed Almutairi <muhnndsaeed@Muhannads-MacBook-Pro.local>
2023-07-30 12:08:24 +0300

    Merge Branches

--- database/migrations/2023_07_30_074039_create_categories_table.php
+++ database/migrations/2023_07_30_074039_create_categories_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
