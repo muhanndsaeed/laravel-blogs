f0215d8c15c19fc10b3091bb627f7f47160c48e8
Muhannad Saeed Almutairi <muhnndsaeed@Muhannads-MacBook-Pro.local>
2023-08-01 13:42:37 +0300

    Merge branches

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use App\Models\User;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
     protected $fillable = [
+        'user_id',
         'title',
         'description',
     ];
