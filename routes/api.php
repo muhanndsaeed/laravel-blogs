793ea62a131a6dc1a1b84777924a2ab35140fe33
Noura Saad <noura.saad1419@gmail.com>
2023-08-01 10:24:13 +0300

    feat:create and migrate post model

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Post\PostController;
 use App\Http\Controllers\API\Category\CategoryController;
 use App\Http\Controllers\API\Authentication\AuthController;
 use App\Http\Controllers\API\Authentication\UserController;
@@ -37,3 +38,6 @@ Route::middleware(['auth:api', 'admin'])->group(function(){
     Route::delete('category/{id}' , [CategoryController::class , 'destroy']);
 
 });
+Route::get('category' , [CategoryController::class , 'index']);
+
+Route::resource('post',PostController::class)->middleware('auth:api');
